e5caede68538fd2e59169840a627bca50514b2c9
Sa-kws <77007516+Sa-kws@users.noreply.github.com>
2021-02-26 10:39:20 +0100

    Update elements.py

--- Essai_GridGeneration_POO/word2grid/elements.py
+++ Essai_GridGeneration_POO/word2grid/elements.py
@@ -1,4 +1,4 @@
-import pandas as pd
+import pandas
 
 class Grid():
     PAGES = []
@@ -12,6 +12,10 @@ class Grid():
     def addPage():
         pass
 
+    def export_to_csv(self, grid, name):
+        df = grid
+        pandas.df.to_csv(name, sep='\t')
+
 
 class Page():
     SLOTS = Grid.SLOTS
@@ -20,29 +24,36 @@ class Page():
         self.Page = Page
 
     def makePage(self):
-        return pd.DataFrame(columns=[str(x + 1) for x in range(0, Slot.COL_NUMBER)], index=range(Slot.ROW_NUMBER)).astype(str)
+        return pandas.DataFrame(columns=[str(x + 1) for x in range(0, Slot.COL_NUMBER)], index=range(Slot.ROW_NUMBER)).astype(str)
 
     def addSlots(self, slots, page):
         # Il faut passer en argument une liste de Slot, on les mettra ensuite dans un tableau
         # Possiblement : DataFrame - pandas
         # Il faut aussi passer une une page, qui correspond à un df qu'on aura créé avec la méthode makePage()
         df = page
-        for col in df.columns:
-            print(col)
-            used_words = []
-            # Erreur : le df se rempli entièrement avec un mot, puis recommence avec le suivant ect...
-            for word in slots:
-                if word in used_words:
-                   pass 
-                else:
-                    used_words.append(word)
-                    df[col] = word
+        for col in df:
+            to_use = []
+            print('--- SLOTS (av) : ', slots, len(slots))
+            to_use = [slots[i] for i in range(0,Slot.ROW_NUMBER)]
+            print('--- TO_USE : ', to_use)
+            df[col] = [x for x in to_use]
+            del slots[0:Slot.ROW_NUMBER]
+            print('--- SLOTS -'+str(Slot.ROW_NUMBER)+ ': ',slots, len(slots))
+        # On passe en argument (slots) la liste de slots qu'on souhaite ajouter à la page, ensuite,
+        # on initialise une liste (to_use) dans laquelle on ajoute le nombre de slots par colonne selon le nombre
+        # de ligne (Slot.ROW_NUMBER). Ensuite, on parcours to_use pour remplir la colonne du DataFrame.
+        # Une fois la colonne remplie, on supp les slots déjà ajoutés de la liste de slots originale pour ne pas
+        # les reprendre dans to_use.
+        #
+        # ?? (On pourrait copier la liste dans la fonction afin de ne pas toucher l'originale)
+
+
         return df
 
 
 class Slot():
-    ROW_NUMBER = 6
-    COL_NUMBER = 3
+    ROW_NUMBER = 3
+    COL_NUMBER = 6
     ITEM_IS_CORE = False
     def __init__(self, row_num, col_num):
         # ceci implique que le numéro de la ligne et de la colonne doit être passé en argument lorsque l'on créé la clase
