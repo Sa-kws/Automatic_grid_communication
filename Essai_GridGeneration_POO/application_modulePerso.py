4c8df9226e65772c4a210649e639b4f307a6bafa
Sa-kws <77007516+Sa-kws@users.noreply.github.com>
2021-02-26 10:40:09 +0100

    Update application_modulePerso.py

--- Essai_GridGeneration_POO/application_modulePerso.py
+++ Essai_GridGeneration_POO/application_modulePerso.py
@@ -1,6 +1,3 @@
-
-#from word2grid import elements as el
-
 # ---------- Création des données à traiter : ----------
 # 2 pages
 pages = ['accueil', 'lieu']
@@ -15,42 +12,14 @@ folder = [x for x in open('vocab_page_accueil.txt', encoding='utf-8') if x.isupp
 # Vocabulaire à placer
 words_page_accueil = [x.replace('\n','') for x in open('vocab_page_accueil.txt', encoding='utf-8') if x.islower()]
 words_page_lieu = [x.replace('\n','') for x in open('vocab_page_lieu.txt', encoding='utf-8') if x.islower()]
-vocab = ['je', 'vouloir', 'aimer', 'quoi', 'ça', 'toi']
-'''
-# ---------- Code à taper dans main() : ----------
-# Initialisation d'une Page :
-page_accueil = el.Page()
-page_accueil = page_accueil.addPage()
-# Transformation des valeurs de la Page en Slots :
-slots_accueil = el.Slot()
-slots_accueil = slots_accueil.addSlots(page_accueil)
-# Ouverture d'une page via un click sur Dossier :
-page_lieu = el.Item()
-page_lieu = page_lieu.clickItem(page_lieu, pages[1])
-
-# ---------- Brouillon : ----------
-
-item = el.Item()
-
-
-
-
-            # Maintenant : df[slot] = le mot
-            # ajout du mot dans une liste (liste_mots_usés)
-            # ajout d'une condition : si le mot est dans la liste mits usés, alors on pass, sinon, df[slot] = le mot suivant
-
-
-#print(page_accueil)
-#print('\n')
-#print(page_lieu)
-
-
-    #put word in slot please :)
-'''
+vocab = ['a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8', 'a9', 'a10', 'a11', 'a12', 'a13', 'A14_F-c', 'a15', 'a16', 'a17', 'A18_F-b']
 
 
 
 from word2grid import new_elements as nel
+
+# ---------- Futur main() : ----------
+
 item = nel.Item('vouloir')
 item = item.makeItem()
 print(item)
@@ -59,10 +28,19 @@ print(slot.position)
 slotitem = slot.addItem(slot.position, item)
 print(slotitem)
 page = nel.Page().makePage()
-print(page)
 filled_slots = nel.Page().addSlots(vocab, page)
+
 print(filled_slots)
 
 
 
-
+# ---------- Brouillon : ----------
+'''
+-------------------------------- Change df value ---XXXXXXXXXXX---
+for i in filled_slots.columns:
+    for val in range(0,len(filled_slots[i])):
+        print(filled_slots[i][val])
+        filled_slots[i][val] = 'try'
+print(filled_slots)
+-------------------------------------------------------------------
+'''
