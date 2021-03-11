82bd7b0121ef0bbe95396c86c00bf515ec39148f
Sa-kws <77007516+Sa-kws@users.noreply.github.com>
2021-03-11 14:24:25 +0100

    Add files via upload

--- Essai_GridGeneration_POO/word2grid/Slot.py
+++ Essai_GridGeneration_POO/word2grid/Slot.py
@@ -1,10 +1,19 @@
+import copy
 class Slot():
 
-    def __init__(self):
-        self.Slot = Slot
+    def __init__(self, word, isCore):
+        # word = str que l'on souhaite traiter et placer sur la grille
+        # is Core = Booléen un indiquant si le mot traité fait parti du vocabulaire core ou non
+        self.__word = copy.copy(word)
+        self.__isCore = copy.copy(isCore)
 
-    def addItem(self, is_core):
-        slot = []
-        slot.append(self)
-        slot.append(is_core)
-        return slot
+    # Accesseur
+    def get_word(self):
+        return self.__word
+
+    # Accesseur
+    def get_is_core(self):
+        return self.__isCore
+
+    def __str__(self):
+        return '('+ str(self.__word) + ',' + str(self.__isCore) + ')'
