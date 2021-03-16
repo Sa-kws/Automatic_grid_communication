9d5a01513b06d7b94a6df286e7934d2aaef2549a
Sa-kws <77007516+Sa-kws@users.noreply.github.com>
2021-03-16 14:51:50 +0100

    Update 1)_GetESLOTextTransciptions.py

--- 1)_GetESLOTextTransciptions.py
+++ 1)_GetESLOTextTransciptions.py
@@ -1,7 +1,4 @@
 # -*- coding: cp1252 -*-
-import time
-begin = time.time()
-
 import re
 
 reg1 = r'<[\s]*[A-Z]*[a-z]*[</]*'
@@ -11,13 +8,7 @@ fichier = open('name.txt', 'w', encoding='utf-8')
 with open('ESLO2_BOUL_1250_C.trs') as trs:
     for o in trs:
         if re.match(reg1, o) or o == '\n':
-            print(o.replace('\n',''))
             pass
         else:
             fichier.write(o)
 fichier.close()
-
-end = time.time()
-temps = end-begin
-minutes = round((temps / 60),2)
-print('Temps d\'execution : '+str(minutes)+' minute.s.')
