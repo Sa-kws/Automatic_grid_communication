cbf5eb977b22bcc7fdee92aa860e75e99b5ef2f1
Sa-kws <77007516+Sa-kws@users.noreply.github.com>
2021-03-16 15:06:45 +0100

    Update 1)_GetESLOTextTransciptions.py

--- 1)_GetESLOTextTransciptions.py
+++ 1)_GetESLOTextTransciptions.py
@@ -8,14 +8,11 @@ fichier = open('name.txt', 'w', encoding='utf-8')
 # Version 1
 with open('ESLO2_BOUL_1250_C.trs') as trs:
     for o in trs:
-        if re.match(reg1, o) or o == '\n':
-            pass
-        else:
+        if re.match(reg1, o) == None and o != '\n':
             fichier.write(o)
 
-
 # Version 2
-annotations = [0 if re.match(reg1, o) or o == '\n' else fichier.write(o) for o in open('ESLO2_BOUL_1250_C.trs')]
-del annotations
+transcriptions = [fichier.write(o) for o in open('ESLO2_BOUL_1250_C.trs') if re.match(reg1, o) == None and o != '\n']
+del transcriptions
 
 fichier.close()
