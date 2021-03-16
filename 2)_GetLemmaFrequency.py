daddc52ab6422aafb04b3f66112921c40f472022
Sa-kws <77007516+Sa-kws@users.noreply.github.com>
2021-03-16 16:47:45 +0100

    Update 2)_GetLemmaFrequency.py

--- 2)_GetLemmaFrequency.py
+++ 2)_GetLemmaFrequency.py
@@ -2,19 +2,25 @@ import time
 begin = time.time()
 
 import spacy
+import re
+
+lemme = ''
+with open('name.txt', 'r', encoding='utf-8') as f:
+    for ligne in f:
+        ligne = re.sub(r'[^\w\s]', '', ligne)
+        lemme += ligne.replace('\n', ' ')
 
 frequence = {}
+doc = nlp(lemme)
+    for token in doc:
+        if token.lemma_ in frequence:
+            frequence[token.lemma_] += 1
+        else:
+            frequence[token.lemma_] = 1
 
-with open('name.txt', 'r', encoding='utf-8') as f:
-    for i in f:
-        doc = nlp(i.replace('\n',''))
-        for token in doc:
-            if token.lemma_ in frequence:
-                frequence[token.lemma_] += 1
-            else:
-                frequence[token.lemma_] = 1
 frequence = sorted(frequence.items(), key=operator.itemgetter(1))
 frequence.reverse()
+
 with open('Vocab_ESLO.txt', 'w', encoding='utf-8') as out:
     for key, value in frequence:
         out.write(str(key) + '\t' + str(value) + '\n')
