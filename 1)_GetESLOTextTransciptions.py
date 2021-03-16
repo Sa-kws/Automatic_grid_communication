c38aa48c603f65558ce41d0cb7b8fb5979db4fcf
Sa-kws <77007516+Sa-kws@users.noreply.github.com>
2021-03-16 14:50:24 +0100

    Update 1)_GetESLOTextTransciptions.py

--- 1)_GetESLOTextTransciptions.py
+++ 1)_GetESLOTextTransciptions.py
@@ -1,45 +1,21 @@
+# -*- coding: cp1252 -*-
 import time
 begin = time.time()
 
-import glob
-import os
 import re
-import codecs
 
-reg1 = r'<[\s]*[A-Z]*[a-z]'
-reg2 = r'</'
+reg1 = r'<[\s]*[A-Z]*[a-z]*[</]*'
+transcriptions = []
 
-
-os.mkdir("ESLO_modified")
-dossier = glob.glob('ESLO_2//*')
-
-
-
-'''
-with codecs.open('ESLO_2/ESLO2_BOUL_1250_C.trs', 'r', encoding='mbcs') as ansi_file:
-    with codecs.open("ESLO_BOUL_utf8_test.txt", "w", encoding="UTF-8") as utf_8file:
-        while True:
-            contents = ansi_file.read(blockSize)
-            if not contents:
-                break
-            utf_8file.write(contents)
-            
-'''
-
-for fichier in dossier:
-    transcriptions = []
-    name = fichier.replace('ESLO_2\\', '')
-    name = name.replace('.trs', '.txt')
-    name = 'ESLO_modified/' + name
-    with codecs.open(fichier, 'r', encoding='mbcs') as f:
-        for o in f:
-            o = o.replace('\t', '',6)
-            if re.match(reg1, o) or re.match(reg2, o): pass
-            elif o == '\n': pass
-            else: transcriptions.append(o)
-    with codecs.open(name, 'w', encoding='utf-8') as out:
-        for enonce in transcriptions:
-            out.write(enonce)
+fichier = open('name.txt', 'w', encoding='utf-8')
+with open('ESLO2_BOUL_1250_C.trs') as trs:
+    for o in trs:
+        if re.match(reg1, o) or o == '\n':
+            print(o.replace('\n',''))
+            pass
+        else:
+            fichier.write(o)
+fichier.close()
 
 end = time.time()
 temps = end-begin
