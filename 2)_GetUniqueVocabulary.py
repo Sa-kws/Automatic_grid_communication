cfb3dcad18cfc4ecb39ef4e7a651f264e731c534
Sa-kws <77007516+Sa-kws@users.noreply.github.com>
2021-03-17 09:56:08 +0100

    Update 2)_GetUniqueVocabulary.py

--- 2)_GetUniqueVocabulary.py
+++ 2)_GetUniqueVocabulary.py
@@ -1,9 +1,11 @@
 from collections import OrderedDict
 import re
 
+name_of_file = 'Wiki_Fleur.txt'
+
 # version 1 - sortie : une grande liste
 donnees = ''
-with open('Wiki_Fleur.txt', 'r', encoding='utf-8') as fichier:
+with open(, name_of_file, 'r', encoding='utf-8') as fichier:
     for ligne in fichier:
         donnees += re.sub(r'[^\w\s]', '', ligne.lower().replace('\n', ' '))
 cleaned = list(OrderedDict.fromkeys(donnees.split(' ')))
@@ -11,7 +13,7 @@ print(cleaned)
 
 
 # Version 2 - sortie : une liste par ligne
-donnees = [ligne.lower().replace('\n', ' ') for ligne in open('Wiki_Fleur.txt', 'r', encoding='utf-8')]
+donnees = [ligne.lower().replace('\n', ' ') for ligne in open(name_of_file, 'r', encoding='utf-8')]
 donnees = [re.sub(r'[^\w\s]', '', x).split(' ') for x in donnees]
 cleaned = [list(OrderedDict.fromkeys(x)) for x in donnees]
 print(cleaned)
